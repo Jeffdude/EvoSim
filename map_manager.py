e174a1cf19e1bcc9b6a78b5b4baf424281075000
Jeffdude <jeffmilling@gmail.com>
2016-05-16 19:47:31 +0200

    sightDistance working for bugs with multiple eyes

--- map_manager.py
+++ map_manager.py
@@ -32,6 +32,8 @@ class map_manager:
 
         bug: bug object used for point of origin
         direction: the direction of sight (in radians)
+        getID: if True (default: False) return a list of stick id's instead
+            of the stick objects
         """
 
         bug_x, bug_y = bug.location
@@ -65,9 +67,9 @@ class map_manager:
         else:
             return colliders
 
-    def sightDistance(self, bug, direction):
+    def intersectPoints(self, bug, direction):
         """
-        bug looks in direction (radians)
+        eye looks in direction (radians)
 
         returns a floating point representation of the distance to the nearest
         stick in the bugs vision vector
@@ -76,17 +78,9 @@ class map_manager:
         bug_x, bug_y = bug.location
         v_slope = - math.tan(direction) 
 
-        if direction > 0 and direction < math.pi:
-            looking_up = True
-        else:
-            looking_up = False
-
-        if ((direction < (math.pi / 2.0) and direction >= 0)
-            or (direction <= (2 * math.pi) 
-                and direction > ((3.0 * math.pi) / 2.0))):
-            looking_right = True
-        else:
-            looking_right = False
+        # filter booleans
+        looking_up = (direction > 0 and direction < math.pi)
+        looking_right = (math.cos(direction) > 0)
 
         bug_b = bug_y - v_slope * bug_x # b = y - m*x
         int_pnts = []
@@ -96,20 +90,17 @@ class map_manager:
 
             stk_dict = stk_details[stk_id]
             stk = stk_dict['stick']
-
-            # filter the colliders on opposite side of vision
-            if ((stk.location[1] - stk_dict['height']) > bug_y 
-                    and looking_up):
-                continue # skip
-            elif (stk.location[1] + stk_dict['height']) < bug_y and not looking_up:
-                continue # skip
-
-            # skip debug stick
-            if stk_id is 0:
-                continue
-
             start_x, start_y = stk_dict['start']
             stop_x, stop_y = stk_dict['stop']
+
+            # filter the colliders on opposite side of vision
+            if (start_y > bug_y and stop_y > bug_y 
+                    and looking_up):
+                continue # skip
+            elif (start_y < bug_y and stop_y < bug_y 
+                    and not looking_up):
+                continue # skip
+
             stk_slope = float(start_y - stop_y) / float(start_x - stop_x)
             stk_b = start_y - stk_slope * start_x  # b = y - m * x
             """
@@ -123,6 +114,7 @@ class map_manager:
             int_y = bug_b + v_slope * int_x
             #int_y = stk_b + stk_slope * int_x
             int_pnts.append((int(int_x), int(int_y)))
+
         new_pnts = []
         for pnt_x, pnt_y in int_pnts:
             if (((pnt_y > bug_y) and looking_up)
@@ -132,3 +124,48 @@ class map_manager:
                     continue
             new_pnts.append((pnt_x, pnt_y))
         return (new_pnts, colliders_id)
+    
+    def diffDistance(self, coordA, coordB):
+        """
+        helper function returns simple distance between two coordinates
+        """
+        xA, yA = coordA
+        xB, yB = coordB
+        dsquared = (float(xA - xB)**2) + (float(yA-yB)**2)
+        return math.sqrt(dsquared)
+
+
+    def sightDistances(self, bug):
+        """
+        returns dict of mappings of the bug's eye ID and floating point
+        representations of the distance to the nearest stick within the vision
+        vector of each
+
+        if the eye_id does not see anything in it's range, it's distance value
+        will be -1
+        """
+        distances = {}
+        tot_int_pnts_in_range = []
+        tot_colliders_id = []
+        for eye_id in bug.eyes.keys():
+            eye = bug.eyes[eye_id]
+            min_distance = float('inf')
+            # check the intercept points for the closest
+            int_coord_list, colliders_id = self.intersectPoints(bug, eye.direction)
+            int_pnts_in_range = []
+            for int_coord in int_coord_list:
+                distance = self.diffDistance(bug.location, int_coord) 
+                if distance < eye.length:
+                    int_pnts_in_range.append(int_coord)
+                    min_distance = min(distance, min_distance)
+            # eye sees nothing
+            if min_distance == float('inf'):
+                min_distance = -1
+
+            # add the eye's return value
+            distances[eye_id] = min_distance
+            # aggregate intercept points and colliders
+            tot_int_pnts_in_range += int_pnts_in_range
+            tot_colliders_id += colliders_id
+        return (distances, tot_int_pnts_in_range, tot_colliders_id)
+                
